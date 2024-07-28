46a45dacbfcc4e3a0dc621a04279dee49139fedf
Tegveer Singh Bhatia <sanjam011202@gmail.com>
2024-07-28 22:52:10 +0530

    Added Shooting and bullets

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float damageAmount = 15f;
+
+    Rigidbody2D rb;
+    public float bulletSpeed;
+
+    public float speed = 10f;
+    private Vector2 moveDirection;
+
     private void OnCollisionEnter(Collision other) {
         IDamagable damageAble = other.gameObject.GetComponentInParent<IDamagable>();
         if(damageAble != null && other.gameObject.tag == "Astroid"){
             damageAble.Damage(damageAmount);
         }
         Destroy(gameObject);
+    }
+    public void SetMoveDirection(Vector2 direction)
+    {
+        moveDirection = direction.normalized * speed;
+    }
+
+    void Update()
+    {
+        transform.Translate(moveDirection * Time.deltaTime);
     }
 }
